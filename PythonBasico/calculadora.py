181a20aa030d3225662f9637c76f29026f27257c
Alice da Silva Costa <a.costa@easysoft.interno>
2020-10-29 14:00:35 -0300

    .

---
+++ PythonBasico/calculadora.py
@@ -0,0 +1,28 @@
+# Calcule o resto da divisão de 10 por 3.
+#print (10%3)
+
+
+
+# Calcule a tabuada do 13.
+#cont = 0
+
+#while cont < 11:
+    #mult = 13 * cont
+    #print ('13 x ' + str(cont) + ' = ' + str(mult))
+
+    #cont = cont + 1
+
+
+
+# Davinir não gosta de ir às aulas. Mas ele é obrigado a comparecer a pelo menos 75% delas. 
+# Ele quer saber quantas aulas pode faltar, sabendo que tem duas aulas por semana, durante quatro meses. 
+# Ajude o Davinir!
+
+diasAula = (2**4)*4
+print (diasAula)
+falta = diasAula*0.75
+
+print(falta)
+
+
+    
